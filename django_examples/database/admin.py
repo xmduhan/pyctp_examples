e6e1b4725e7ae65f3d7eacfc5c0572a9a95ff6ae
xmduhan <xmduhan@gmail.com>
2015-08-25 17:50:18 +0800

    ...

--- django_examples/database/admin.py
+++ django_examples/database/admin.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
 # Register your models here.
+
+from models import *
+
+class DepthMarketDataAdmin(admin.ModelAdmin):
+    """
+    行情数据编辑显示定义
+    """
+    fields = [i.name for i in ModelDepthMarketData._meta.fields[1:]]
+
+admin.site.register(ModelDepthMarketData,DepthMarketDataAdmin)
